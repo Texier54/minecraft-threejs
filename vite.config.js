dc2d6f14aab08012979bc185cf3530164849eb67
Baptiste Texier <baptiste.texier@missnumerique.com>
2025-02-15 15:20:14 +0100

    build

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: "/minecraft-threejs/", // Remplace par le nom de ton repo GitHub
+    build: {
+        outDir: "dist"
+    }
+});
