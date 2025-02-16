3052557d103acee3c01f88f0a229c68e52f958ec
Baptiste Texier <baptiste.texier@missnumerique.com>
2025-02-16 15:44:15 +0100

    add list build

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # minecraft-threejs
+
+Réplique du jeu minecraft avec ThreeJS
+
+## Live Demo
+
+https://texier54.github.io/minecraft-threejs/
+
+## Control 
+
+* ZQSD -> Mouvement
+* E -> inventaire
+* Echap -> menu
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: "/minecraft-threejs/", // Remplace par le nom de ton repo GitHub
     build: {
-        outDir: "dist"
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                craftList: 'craftList.html'
+            }
+        }
     }
 });
